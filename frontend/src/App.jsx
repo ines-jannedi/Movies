011c537e54def182c3dad6dae8dbe361ac6f69eb
ines jannedi <inesjannedi11@gmail.com>
2024-12-19 00:31:49 +0200

    Auth screen UI Design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,22 @@
 
 import { Routes,Route } from 'react-router-dom'
-import HomePage from './pages/HomePage.jsx'
+import HomePage from './pages/home/HomePage.jsx'
 import SignUpPage from './pages/SignUpPage.jsx'
 import LoginPage from './pages/LoginPage.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
   
 
   return (
+    <>
     <Routes>
      <Route path="/" element={<HomePage/>}/>
      <Route path="/login" element={<LoginPage/>}/>
      <Route path="/signup" element={<SignUpPage/>}/>
     </Routes>
+    <Footer/>
+    </>
   )
 }
 
---
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -0,0 +1,11 @@
+
+
+function HomeScreen() {
+  return (
+    <div>
+      homescreen
+    </div>
+  )
+}
+
+export default HomeScreen
