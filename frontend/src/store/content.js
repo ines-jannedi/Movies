50c5f4c84d42e47ded8fed4eb7a7b82ca3d12d63
ines jannedi <inesjannedi11@gmail.com>
2024-12-27 01:05:08 +0200

    building the home Screen

---
+++ frontend/src/store/content.js
@@ -0,0 +1,9 @@
+// tv or movie
+
+import { create } from "zustand";
+
+
+export const useContentStore = create ((set)=>({
+contentType:"movie",
+setContentType: (type) => set ({contentType:type}),
+}))
